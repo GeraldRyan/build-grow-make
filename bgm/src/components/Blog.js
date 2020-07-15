ac5ae07b8c245a0c27ca30b8fbda82bd77f796e2
GeraldRyan <gerald.ryan40@yahoo.com>
2020-07-15 13:03:47 -0500

    making blog get state from an object. I am remembering and building from scratch, the way it has to be, and blogging about my personal interests

--- bgm/src/components/Blog.js
+++ bgm/src/components/Blog.js
@@ -1,12 +1,15 @@
 import React from "react";
-import {Blogposts} from '../assets/Blogposts'
+import { Blogposts } from "../assets/Blogposts";
 
 const Blog = () => {
   return (
     <div id="blogdiv">
       My Blog
-      <div>{Blogposts[1]["title"]}</div>
       <br/>
+      <br/>
+      <div>{Blogposts[1]["title"]}</div>
+      <div>{Blogposts[1]["body"]}</div>
+      <br />
     </div>
   );
 };
