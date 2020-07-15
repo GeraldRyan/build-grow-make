f55adb5283f41d7ffc91022066aaa943a7ee9981
GeraldRyan <gerald.ryan40@yahoo.com>
2020-07-14 19:17:44 -0500

    changed logo

--- bgm/src/App.js
+++ bgm/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import tool from './crossedtools.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={tool} className="App-logo" alt="logo" />
         <p className="pre">
           Build    Grow    Make
         </p>
