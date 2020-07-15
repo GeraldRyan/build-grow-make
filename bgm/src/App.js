56d6f90eed47d3138abad34fbdb352367608c970
GeraldRyan <gerald.ryan40@yahoo.com>
2020-07-14 23:13:40 -0500

    update. i hate designing websites. not for me

--- bgm/src/App.js
+++ bgm/src/App.js
@@ -1,27 +1,28 @@
 import React from "react";
-import logo from "./logo.svg";
-import tool from "./crossedtools.svg";
+// import logo from "./logo.svg";
+import gearimg from "./crossedtools.svg";
 import "./App.css";
-import Blog from './components/Blog'
+
+import { Route, Link, Switch } from "react-router-dom";
+import Blog from "./components/Blog";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="pre banner flexed-centered">
-          <p>Build </p>
-          <p>Grow </p>
-          <p>Make</p>
-        </div>
-        <img src={tool} className="App-logo" alt="logo" />
+        <div className="pre banner flexed-centered">Build Grow Make</div>
+        <img src={gearimg} className="App-logo" alt="logo" />
       </header>
       <body className="App-body">
         <div className="pre flexed-centered">
-          <a>Projects    </a>
-          <a href="/blog">Blog    </a>
-          <a>About</a>
+          <a href="/">Home </a>
+          <a href="/projects</div>">Projects </a>
+          <a href="/blog">Blog </a>
+          <a href="/about">About</a>
         </div>
-        <Blog />
+        <Route path="/blog">
+          <Blog />
+        </Route>
         {/* <a
           className="App-link"
           href="https://reactjs.org"
@@ -30,7 +31,6 @@ function App() {
         >
           Enter Site
         </a> */}
-
       </body>
     </div>
   );
--- bgm/src/components/Blog.js
+++ bgm/src/components/Blog.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Gears from "../assets/gears.jpg";
-import {BrowserRouter as Router} from 'react-router-dom'
 
 const Blog = () => {
   return (
     <div id="blogdiv">
-      Blog
-      <img src={Gears} alt="" />
+      
+      <br/>
+      <img src={Gears} alt="Gold Gears" id="gears" />
     </div>
   );
 };
